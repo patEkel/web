a0437512ce0b33f508c3d200b1b023eef7b54b49
Patrick Ekel <patrick.ekel@utah.edu>
2017-08-04 21:59:15 -0600

    panel functional

--- index.php
+++ index.php
@@ -403,19 +403,21 @@
       $('.navbar-collapse').hide();
       active = false;
     }});
+    </script>
 
+    <script>
     /*Scipt for code accordians*/
     var acc = document.getElementsByClassName("accordion");
     var i;
 
-    for (i = 0; i &lt; acc.length; i++) {
+    for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
     this.classList.toggle("active");
 
     /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
+     var panel = this.nextElementSibling;
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
